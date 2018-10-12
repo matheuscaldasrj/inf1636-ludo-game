7a68c011e6543ae0fb1c525b83bbf2139953d415
Matheus Caldas <matheuscaldasrj@gmail.com>
2018-10-12 13:31:06 -0300

    - adding aspect ratio

--- src/layout/BoardPanel.java
+++ src/layout/BoardPanel.java
@@ -1,9 +1,11 @@
 package layout;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.Rectangle2D;
+import java.awt.geom.Rectangle2D.Float;
 import java.util.Iterator;
 
 import javax.swing.JPanel;
@@ -11,8 +13,8 @@ import javax.swing.JPanel;
 public class BoardPanel extends JPanel {
 	
 	private Graphics2D graphics2;
-	private float initialSquareSize = 320;
-	float centerSquareHeigth;
+	private float initialSquareSize;
+	float centerSquareSize;
 	
 	public BoardPanel() {
 	}
@@ -23,49 +25,60 @@ public class BoardPanel extends JPanel {
 		super.paintComponent(g);		
 		graphics2 = (Graphics2D) g;		
 		
-		//TODO 
-		//understand why this setSize is necessary
-//		setSize(800,800);
-		System.out.println("paintComponent construtor");
-		System.out.println(getSize());
-		
-		paintInitialSquares();
-//		paintBoardSquares();
+		keepAspectRatio();
 
+		initialSquareSize = (float) (getSize().getWidth() * 2/ 5);
+
+		paintInitialSquares();
+		paintBoardSquares();
+
+	}
+	
+	private void keepAspectRatio() {
+		//lets keep aspect ratio
+		int maxSize;
+		if(getSize().getHeight() >= getSize().getWidth()) {
+			maxSize = (int) getSize().getWidth();
+		} else {
+			maxSize = (int) getSize().getHeight();
+		}
+		
+		setSize(maxSize,maxSize);
 	}
 	
 	private void paintBoardSquares() {
 		
 		drawCenterRectangule();		
 		
-		drawGreenTeam();
+
+//		drawGreenTeam();
 		drawBlueTeam();
-		drawRedTeam();
-		drawYellowTeam();
+//		drawRedTeam();
+//		drawYellowTeam();
 		
 
 	}
 	
 	private void drawYellowTeam() {
-		drawGameSquares(initialSquareSize + centerSquareHeigth, initialSquareSize, true);
+		drawGameSquares(initialSquareSize + centerSquareSize, initialSquareSize, false, Color.YELLOW);
 	}
 	
 	private void drawRedTeam() {
-		drawGameSquares(0, initialSquareSize, true);
+		drawGameSquares(0, initialSquareSize, false, Color.RED);
 	}
 	
 	private void drawGreenTeam() {
-		drawGameSquares(initialSquareSize, 0, false);
+		drawGameSquares(initialSquareSize, 0, true, Color.GREEN);
 	}
 	private void drawBlueTeam() {
-		drawGameSquares(initialSquareSize, initialSquareSize  + centerSquareHeigth, false);	
+		drawGameSquares(initialSquareSize, initialSquareSize  + centerSquareSize, true, Color.BLUE);	
 	}
 		
-	private void drawGameSquares(float xPosition, float yPosition, boolean isVertical) {
+	private void drawGameSquares(float xPosition, float yPosition, boolean isVertical, Color color) {
 		
 		float rectWidth;
 			
-		if(!isVertical) {
+		if(isVertical) {
 			rectWidth = (float) (getSize().getWidth() - 2*initialSquareSize) / 3;
 		} else {
 			rectWidth = (float) (getSize().getHeight() - 2*initialSquareSize) / 3;
@@ -81,23 +94,26 @@ public class BoardPanel extends JPanel {
 			float rectX;
 			float rectY;
 			
-			if(!isVertical) {
+			if(isVertical) {
 				rectX = xPosition + rectWidth*index;;
 				rectY = yPosition;				
 			} else {
 				rectX = xPosition;
 				rectY = yPosition + rectWidth*index;
 			}
-			
-
+		
 			rect = new Rectangle2D.Float(rectX, rectY, rectWidth, rectWidth);
+			
+			
+			
+			rect = fillColor(rect,i,color);
+			
 			graphics2.draw(rect);				
 			index++;
 			
 			
 			if(index != 0 && index % 3 == 0) {
-				System.out.println(i);
-				if(!isVertical) {
+				if(isVertical) {
 					yPosition += rectWidth;
 					xPosition = initialSquareSize;	
 				} else {
@@ -107,17 +123,31 @@ public class BoardPanel extends JPanel {
 				
 				index = 0;
 			}
-				
+					
 		}
 	}
 	
-
-	
+	private Float fillColor(Rectangle2D.Float rect, int index, Color color) {
+		//checks using index if it should be colored;
+		
+		if(index == 2) {
+			graphics2.setColor(color);
+			graphics2.fill(rect);
+		}
+		
+		
+		//back to black
+		graphics2.setPaint(Color.BLACK);
+		
+		return rect;
+	}
 	private void drawCenterRectangule() {
-		float centerSquareWidth = (float) (getSize().getWidth() - 2*initialSquareSize) -1;
-		centerSquareHeigth = (float) (getSize().getHeight() - 2*initialSquareSize);
-		Rectangle2D.Float rect = new Rectangle2D.Float(initialSquareSize, initialSquareSize - 1, centerSquareWidth,centerSquareHeigth);
-		graphics2.setColor(Color.YELLOW);
+		System.out.println("height: " + getSize().height);
+
+		System.out.println("getWidth: " + getSize().getWidth());
+		centerSquareSize = (float) (getSize().getHeight() - 2*initialSquareSize);
+		Rectangle2D.Float rect = new Rectangle2D.Float(initialSquareSize, initialSquareSize , centerSquareSize,centerSquareSize);
+		graphics2.setColor(Color.BLACK);
 		graphics2.fill(rect);
 		graphics2.draw(rect);
 	}
@@ -125,24 +155,24 @@ public class BoardPanel extends JPanel {
 	
 	private void paintInitialSquares() {
 				
-		float rectWidth = (float) (getSize().getWidth() - initialSquareSize);
-		float rectHeight = (float) (getSize().getHeight() - initialSquareSize);
+		float rectX = (float) (getSize().getWidth() - initialSquareSize);
+		float rectY = (float) (getSize().getHeight() - initialSquareSize);
 		
 		Rectangle2D.Float  rect1 = new Rectangle2D.Float(0, 0, initialSquareSize, initialSquareSize);
 		graphics2.setColor(Color.RED);
 		graphics2.fill(rect1);
 		
 		
-		Rectangle2D.Float  rect2 = new Rectangle2D.Float(rectWidth, 0, initialSquareSize, initialSquareSize);
+		Rectangle2D.Float  rect2 = new Rectangle2D.Float(rectX, 0, initialSquareSize, initialSquareSize);
 		graphics2.setColor(Color.GREEN);
 		graphics2.fill(rect2);
 		
 		
-		Rectangle2D.Float  rect3 = new Rectangle2D.Float(0, rectHeight, initialSquareSize, initialSquareSize);
+		Rectangle2D.Float  rect3 = new Rectangle2D.Float(0, rectY, initialSquareSize, initialSquareSize);
 		graphics2.setColor(Color.BLUE);
 		graphics2.fill(rect3);
 		
-		Rectangle2D.Float  rect4 = new Rectangle2D.Float(rectWidth, rectHeight , initialSquareSize, initialSquareSize);
+		Rectangle2D.Float  rect4 = new Rectangle2D.Float(rectX, rectY , initialSquareSize, initialSquareSize);
 		graphics2.setColor(Color.YELLOW);
 		graphics2.fill(rect4);
 	}
