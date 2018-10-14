9f92e0b978bea7cec6648ea3cf79fd4519629121
Bernardo Hörner Lopes <behornerl@gmail.com>
2018-10-14 17:22:54 -0300

    - Corrected typo "WIDHT_DEFAULT" to "WIDTH_DEFAULT"

--- src/layout/LudoGameFrame.java
+++ src/layout/LudoGameFrame.java
@@ -10,7 +10,7 @@ import javax.swing.JSplitPane;
 
 public class LudoGameFrame extends JFrame {
 	
-	private final int WIDHT_DEFAULT=1200;
+	private final int WIDTH_DEFAULT=1200;
 	private final int HEIGHT_DEFAULT=800;
 
 	
@@ -29,10 +29,10 @@ public class LudoGameFrame extends JFrame {
 		JPanel controlPanel = buildControlPanel();
 		
 		JSplitPane splitPane = new JSplitPane();
-        splitPane.setSize(WIDHT_DEFAULT, HEIGHT_DEFAULT);
+        splitPane.setSize(WIDTH_DEFAULT, HEIGHT_DEFAULT);
         splitPane.setDividerSize(0);
 
-        splitPane.setDividerLocation(WIDHT_DEFAULT*2/3);
+        splitPane.setDividerLocation(WIDTH_DEFAULT*2/3);
         splitPane.setOrientation(JSplitPane.HORIZONTAL_SPLIT);
         splitPane.setLeftComponent(boardPanel);
         splitPane.setRightComponent(controlPanel);
@@ -43,7 +43,7 @@ public class LudoGameFrame extends JFrame {
 	
 	private JPanel buildBoardPanel() {
 		BoardPanel boardPanel = new BoardPanel();
-		boardPanel.setSize(WIDHT_DEFAULT*2/3,HEIGHT_DEFAULT);
+		boardPanel.setSize(WIDTH_DEFAULT*2/3,HEIGHT_DEFAULT);
 		boardPanel.setBackground(Color.WHITE);
 		System.out.println("boardSize");
 		System.out.println(boardPanel.getSize());
@@ -52,20 +52,20 @@ public class LudoGameFrame extends JFrame {
 		
 	private JPanel buildControlPanel() {
 		ControlPanel controlPanel = new ControlPanel();
-		controlPanel.setSize(WIDHT_DEFAULT,HEIGHT_DEFAULT);
+		controlPanel.setSize(WIDTH_DEFAULT,HEIGHT_DEFAULT);
 		controlPanel.setBackground(Color.RED);		
 		return controlPanel;
 	}
 	
 	private void setLayoutPositions() {
-		setSize(WIDHT_DEFAULT,HEIGHT_DEFAULT);
+		setSize(WIDTH_DEFAULT,HEIGHT_DEFAULT);
 		Toolkit toolkit =Toolkit.getDefaultToolkit();
 		Dimension screenSize = toolkit.getScreenSize();
 		int sl=screenSize.width;
 		int sa=screenSize.height;
-		int x=sl/2-WIDHT_DEFAULT/2;
+		int x=sl/2-WIDTH_DEFAULT/2;
 		int y=sa/2-HEIGHT_DEFAULT/2;
-		setBounds(x,y,WIDHT_DEFAULT,HEIGHT_DEFAULT);
+		setBounds(x,y,WIDTH_DEFAULT,HEIGHT_DEFAULT);
 		
 	}
 	
